88ea62afa53ca0c00e4e1c281a426bbaaaf3d14a
Jimmy-WSU <wen-chih.li@wsu.edu>
2021-04-19 21:37:22 +0800

    update code

--- example of core-peripheryu-structures.py
+++ example of core-peripheryu-structures.py
@@ -2,13 +2,18 @@ import cpnet
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+
 G = nx.karate_club_graph()
 alg = cpnet.BE() # Load the Borgatti-Everett algorithm
-alg.detect(G) # Give the network as an input
-x = alg.get_coreness()  # Get the coreness of nodes
-c = alg.get_pair_id()  # Get the group membership of nodes
-print(x)
+alg = cpnet.MINRES()
+alg.detect(G)
+kmconfig = cpnet.KM_config()
+kmconfig.detect(G)
+c = kmconfig.get_pair_id()
+x = kmconfig.get_coreness()
+print(c)
 fig = plt.figure(figsize=(8, 6))
 ax = plt.gca()
 ax, pos = cpnet.draw(G, c, x, ax)
+ax, _ = cpnet.draw(G, c, x, ax, pos=pos)
 plt.show()
