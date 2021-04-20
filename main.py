fb125795414a0de7eaf4eb8ac81ab8641da6081b
Jianqiao-WSU <jianqiao.liu@wsu.edu>
2021-04-20 20:56:37 +0800

    read bitcoin network

--- main.py
+++ main.py
@@ -5,27 +5,19 @@ import pandas as pd
 import cpnet
 
 
-df = pd.read_csv('dataset/soc-sign-bitcoinotc.csv', sep=',', names=['from', 'to', 'rate'])  # load data from dataset
-# print(df)
-df=nx.from_pandas_adjacency(df)
+# df = pd.read_csv('dataset/soc-sign-bitcoinotc-with no time.csv', sep=',', names=['from', 'to', 'rate'], index_col=0)  # load data from dataset
+df = pd.read_csv('dataset/soc-sign-bitcoinotc-with no time and rate.csv', sep=',',usecols=[0,1], names=['source', 'target'])
+print(df)
+# df=nx.from_pandas_adjacency(df)
 G = nx.from_pandas_edgelist(df)
-
-# print(G)
-M = nx.erdos_renyi_graph(80, 0.1)
-# L = nx.karate_club_graph()
-
-
-
+print(G)
 algorithm = cpnet.KM_config()
-# algorithm.detect(L)
-# c = algorithm.get_pair_id()
-# x = algorithm.get_coreness()
-
-# print(c)
-
-# G = nx.karate_club_graph()
-
+algorithm.detect(G)
 c = algorithm.get_pair_id()
 x = algorithm.get_coreness()
 
-print(c)
+# print(c)
+
+print('Name\tPairID\tCoreness')
+for key, value in sorted(c.items(), key=lambda x: x[1]):
+    print('%s\t%d\t%f' %(key, c[key], x[key]))
