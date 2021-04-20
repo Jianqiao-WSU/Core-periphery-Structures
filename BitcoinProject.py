7a427543dd443bca32efae956c09e03f3004d83b
Parikshit-Panwar-WSU <77930781+Parikshit-Panwar-WSU@users.noreply.github.com>
2021-04-20 13:06:15 +0530

    Add files via upload

---
+++ BitcoinProject.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 00:59:46 2021
+
+@author: parikshitpanwar
+"""
+import cpnet
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from csv import reader
+
+# col_list = ['A', 'B', 'C', 'D']
+# ds = pd.read_csv('/Users/parikshitpanwar/Desktop/Library/Elements of Network Science/Project/Core-periphery-Structures-main/dataset/soc-sign-bitcoinotc.csv')
+
+# listofcols = list(ds.columns)
+
+# mycolist = ['2', '4', '1289241911.72836']
+# print(ds[mycolist])
+
+g = nx.Graph()
+
+with open('/Users/parikshitpanwar/Desktop/Library/Elements of Network Science/Project/Core-periphery-Structures-main/dataset/soc-sign-bitcoinotc.csv') as read_obj:
+    csv_reader = reader(read_obj)
+    for row in csv_reader:
+        # print(row[2])
+        g.add_edge(row[0], row[1], weight = row[2])
+
+print(nx.info(g))
