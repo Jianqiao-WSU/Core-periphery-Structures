4ca17201d34b9acd1a29016f1685fde945007823
Jimmy-WSU <wen-chih.li@wsu.edu>
2021-04-21 13:15:25 +0800

    E-R random graph

--- main.py
+++ main.py
@@ -6,22 +6,33 @@ import cpnet
 
 
 # df = pd.read_csv('dataset/soc-sign-bitcoinotc-with no time.csv', sep=',', names=['from', 'to', 'rate'], index_col=0)  # load data from dataset
+#df = pd.read_csv('C:/Users/ASUS/Desktop/project/Core-periphery-Structures/dataset/soc-sign-bitcoinotc-with-no-time.csv', sep=',',usecols=[0,1], names=['source', 'target'])
 df = pd.read_csv('dataset/soc-sign-bitcoinotc-with-no-time.csv', sep=',',usecols=[0,1], names=['source', 'target'])
 print(df)
 # df=nx.from_pandas_adjacency(df)
 G = nx.from_pandas_edgelist(df)
 # print(G)
 
+# read E-R random graph
+n=40
+p=10
+g = nx.erdos_renyi_graph(n, p, seed=None, directed=False)
+
+
 algorithm = cpnet.KM_config()
 algorithm.detect(G)
 c = algorithm.get_pair_id()
 x = algorithm.get_coreness()
 
-# print(c)
+algorithm.detect(g)
+b = algorithm.get_pair_id()
+y = algorithm.get_coreness()
 
+# print(c)
 print('Name\tPairID\tCoreness')
 for key, value in sorted(c.items(), key=lambda x: x[1]):
     print('%s\t%d\t%f' %(key, c[key], x[key]))
+print (y)
 
 # fig = plt.figure(figsize=(8, 6))
 # ax = plt.gca()
